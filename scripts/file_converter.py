2756451cb1a8d36c5d7b0b64af25d1d3b0d1d2ac
nahom temesgen nadew <nadewnahom8@gmail.com>
2024-12-27 13:42:25 +0000

    file converted to csv form

---
+++ scripts/file_converter.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# File paths
+input_file ="C:\\Users\\nadew\\10x\\week3\\ACIS\\data\\MachineLearningRating_v3.txt"
+output_file ="C:\\Users\\nadew\\10x\\week3\\ACIS\\data\\MachineLearningRating_v4.csv"
+# Load the text file with the '|' delimiter
+df = pd.read_csv(input_file, delimiter='|', low_memory=False)
+
+# Save to CSV
+df.to_csv(output_file, index=False)
+
+print("Conversion completed. Saved to", output_file)
+
