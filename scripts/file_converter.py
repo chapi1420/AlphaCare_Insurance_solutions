72e61e819e6d152157c6cb676be14c5d971e5f5c
nahom temesgen nadew <nadewnahom8@gmail.com>
2024-12-28 21:10:10 +0000

    test made

--- scripts/file_converter.py
+++ scripts/file_converter.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # File paths
 input_file ="C:\\Users\\nadew\\10x\\week3\\ACIS\\data\\MachineLearningRating_v3.txt"
-output_file ="C:\\Users\\nadew\\10x\\week3\\ACIS\\data\\MachineLearningRating_v4.csv"
+output_file ="C:\\Users\\nadew\\10x\\week3\\ACIS\\data\\cleaned_data\\Cleaned_Data_v4.csv"
 # Load the text file with the '|' delimiter
 df = pd.read_csv(input_file, delimiter='|', low_memory=False)
 
